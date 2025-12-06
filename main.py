6300c43f29bd5d6a9ecbedfaa7a4ae4978344f40
史蒂夫 <1427035192@qq.com>
2025-12-06 16:32:01 +0800

    perf：优化main

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ if __name__ == "__main__":
     # 设置随机种子确保可重现性
     set_seed(42)
 
-    # 可以通过修改这里的参数来选择运行模式:
-    # Mode.TRAIN, Mode.CLASSIFICATION, Mode.ROGUE_DEVICE_DETECTION, Mode.PRUNE, Mode.DISTILLATION
-    main(Mode.DISTILLATION)
+    # 通过修改这里的参数来选择运行模式:
+    # 执行训练模式
+    # main(Mode.TRAIN)
+    
+    # 执行分类模式
+    main(Mode.CLASSIFICATION)
+    
+    # 执行恶意设备检测模式
+    # main(Mode.ROGUE_DEVICE_DETECTION)
+    
+    # 执行模型剪枝模式
+    # main(Mode.PRUNE)
+    
+    # 执行知识蒸馏模式
+    # main(Mode.DISTILLATION)
