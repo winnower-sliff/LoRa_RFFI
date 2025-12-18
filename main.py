0cbfcd8418d70a67abe750075aed7e0bae1440fd
w1nn0w3r <1427035192@qq.com>
2025-12-18 17:31:09 +0800

    docs：添加main注释

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """项目主入口文件"""
-from core.config import set_seed, Mode, DistillateMode, NetworkType
+from core.config import set_seed, Mode, DistillateMode, NetworkType, PreprocessType
 from core.controller import main
 
 
@@ -19,3 +19,30 @@ if __name__ == "__main__":
     #      teacher_net_type=NetworkType.RESNET,
     #      student_net_type=NetworkType.LightNet,
     #      distillate_mode=DistillateMode.ALL)
+    
+    # --- 其他模式使用示例 ---
+    # 1. 分类模式：使用已训练的模型对设备进行分类
+    #   默认使用LightNet网络和STFT预处理，可调整net_type和preprocess_type参数
+    #   main(Mode.CLASSIFICATION, net_type=NetworkType.LightNet, preprocess_type=PreprocessType.STFT)
+    #
+    # 2. 恶意设备检测模式：检测未知设备是否为恶意设备
+    #   需要已训练的模型，默认使用ResNet网络
+    #   main(Mode.ROGUE_DEVICE_DETECTION, net_type=NetworkType.RESNET, preprocess_type=PreprocessType.STFT)
+    #
+    # 3. 蒸馏模式的其他选项：
+    #   - 仅执行蒸馏训练: DistillateMode.ONLY_DISTILLATE
+    #   - 仅测试学生模型: DistillateMode.ONLY_TEST
+    #   - 仅进行恶意设备检测: DistillateMode.ONLY_ROGUE
+    #   示例：
+    #   main(Mode.DISTILLATION,
+    #        teacher_net_type=NetworkType.RESNET,
+    #        student_net_type=NetworkType.LightNet,
+    #        distillate_mode=DistillateMode.ONLY_TEST)
+    #
+    # 4. 可选参数说明：
+    #   - net_type: 网络类型，可选值: NetworkType.RESNET, NetworkType.DRSN, NetworkType.MobileNetV1, NetworkType.MobileNetV2, NetworkType.LightNet
+    #   - preprocess_type: 预处理类型，可选值: PreprocessType.IQ, PreprocessType.STFT, PreprocessType.WST
+    #   - is_pca_train: 训练时是否使用PCA降维 (默认为True)
+    #   - is_pca_test: 测试时是否使用PCA降维 (默认为True)
+    #   - teacher_net_type, student_net_type: 蒸馏模式专用
+    #   - distillate_mode: 蒸馏训练模式，可选值: DistillateMode.ALL, DistillateMode.ONLY_DISTILLATE, DistillateMode.ONLY_TEST, DistillateMode.ONLY_ROGUE
